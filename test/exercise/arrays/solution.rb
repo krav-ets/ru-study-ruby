1c9de0455fedd758c5c576ccd188884722c59b94
Evgeny Kravets <kravets@netcube.ru>
2023-01-23 21:47:46 +0300

    implement search and replace methods

--- test/exercise/arrays/solution.rb
+++ test/exercise/arrays/solution.rb
@@ -2,11 +2,21 @@ module Exercise
   module Arrays
     class << self
       def replace(array)
-        array
+        max_num = array.reduce(array[0]) do |acc, num|
+          acc = num if num > acc
+          acc
+        end
+        array.map do |num|
+          num >= 0 ? max_num : num
+        end
       end
 
-      def search(_array, _query)
-        0
+      def search(arr, target, left = 0, right = arr.length - 1)
+        mid = (left + right) / 2
+        return mid if arr[mid] == target
+        return -1 if left >= right
+
+        arr[mid] < target ? search(arr, target, mid + 1, right) : search(arr, target, left, mid)
       end
     end
   end
